cf882a46d13cbef77248d27bf79b331a1dc40b6f
Michał Terczyński <mterczynski1@gmail.com>
2024-07-09 20:20:38 +0200

    Update ESLint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  languageOptions: {
+    "parser": "@typescript-eslint/parser",
+  },
+  "parserOptions": {
+    "ecmaVersion": 13,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "ignores": [
+    "webpack.config.js"
+  ],
+  "rules": {
+  }
+}
