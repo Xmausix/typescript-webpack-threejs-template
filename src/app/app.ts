9c954bf1af5f81560e1eaa37d3215043c59dd013
Jakub <148567047+Xmausix@users.noreply.github.com>
2026-03-15 22:11:33 +0100

    Use renderer.setAnimationLoop for render loop

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,24 +4,27 @@ import {
   Scene,
   Vector3,
   WebGLRenderer,
+  Timer,
 } from "three";
 import { Brick } from "./brick";
 
 export class App {
+  private readonly timer = new Timer();
   private readonly scene = new Scene();
+
   private readonly camera = new PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
     0.1,
     10000
   );
+
   private readonly renderer = new WebGLRenderer({
     antialias: true,
     canvas: document.getElementById("main-canvas") as HTMLCanvasElement,
   });
 
   private brick: Brick;
-  private lastTime: number;
 
   constructor() {
     this.brick = new Brick(100, new Color("rgb(255,0,0)"));
@@ -34,26 +37,15 @@ export class App {
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setClearColor(new Color("rgb(0,0,0)"));
 
-    window.addEventListener("resize", () => this.adjustCanvasSize());
-
-    this.lastTime = performance.now(); // initialize lastTime
-    this.render();
+    this.renderer.setAnimationLoop(this.animate);
   }
 
-  private adjustCanvasSize() {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.camera.aspect = window.innerWidth / window.innerHeight;
-    this.camera.updateProjectionMatrix();
-  }
+  private animate = () => {
+    this.timer.update();           // aktualizuje timer
+    const delta = this.timer.getDelta(); // czas od ostatniej klatki
 
-  private render = () => {
-    const now = performance.now();
-    const delta = (now - this.lastTime) / 1000; // convert ms to seconds
-    this.lastTime = now;
-
-    this.brick.rotateY(3 * delta); // rotate based on delta time
+    this.brick.rotation.y += 3 * delta;
 
     this.renderer.render(this.scene, this.camera);
-    requestAnimationFrame(this.render);
   };
 }
